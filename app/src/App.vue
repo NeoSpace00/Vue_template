14ee4390484e933662116484d86e9d7163ceb114
meccah <georgekimiti79@gmail.com>
2024-09-06 14:10:45 +0300

    message

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<section>
+  <div>
+    <RouterView />
+  </div>
+
+<div>
+  <h1  class="text-yellow-400">hello world</h1>
+</div>
+
+
+</section>
+ 
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
